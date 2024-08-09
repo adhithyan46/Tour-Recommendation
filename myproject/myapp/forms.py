9b9e156397406ff3f2a14431b2172fd9a31080f3
adhithyan <youremail@example.com>
2024-08-09 21:29:45 +0530

    login connected

--- myproject/myapp/forms.py
+++ myproject/myapp/forms.py
@@ -4,4 +4,15 @@ class SignupForm(forms.Form):
     email = forms.EmailField(label='Email')
     password = forms.CharField(label='Password', widget=forms.PasswordInput)
     confirm_password = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        confirm_password = cleaned_data.get("confirm_password")
 
+        if password and confirm_password and password != confirm_password:
+            raise forms.ValidationError("Passwords do not match.")
+
+        return cleaned_data
+class LoginForm(forms.Form):
+    username = forms.CharField(max_length=150, required=True)
+    password = forms.CharField(widget=forms.PasswordInput, required=True)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,13 +1,36 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User
-from django.contrib.auth.hashers import make_password
-from . forms import SignupForm
+# from django.contrib.auth.hashers import make_password
+from . forms import SignupForm,LoginForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
+
 
 # from django.template import loader
 def index(request):
     return render(request,'index.html')
 def login_page(request):
-    return render (request,'login_page.html')
+    if request.method == 'POST':       
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']   
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'Login successful.')
+                return redirect('search_page')
+            else:
+                # Display an error message if authentication fails
+                messages.error(request, 'Invalid username or password. Please try again.')
+        else:
+            # Display an error message if the form is invalid
+            messages.error(request, 'Invalid form submission. Please check your input.')        
+    else:
+        form = LoginForm()
+    return render(request, 'login_page.html', {'form': form})
+
 def register_page(request):
     if request.method=='POST':
         form=SignupForm(request.POST)
